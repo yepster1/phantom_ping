56f044fe15bd496bb5ba8e6f9b7a7dd67e7703d6
Cary <yepster1@live.co.za>
2019-06-10 11:27:37 +0200

    made config start and end return ints instead of strings

--- src/config.py
+++ src/config.py
@@ -1,4 +1,3 @@
-
 def get_back_time():
     return 2
 
@@ -9,7 +8,7 @@ def get_raw_directory():
     return "../data/"
 
 def get_start(time):
-    return str(round(time/10) -  get_back_time())
+    return round(time/10) -  get_back_time()
 
 def get_end(time):
-    return str(round(time/10) +  get_forward_time())
+    return round(time/10) +  get_forward_time()
