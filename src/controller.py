278e5e1087c1d7602123cc2719a708f318113f0e
Cary <yepster1@live.co.za>
2019-06-10 11:28:31 +0200

    two threads working, one to recieve pings and one to show videos

--- src/controller.py
+++ src/controller.py
@@ -1,12 +1,43 @@
 import videoCombiner as vc
-import config as config
+import config as c
+import cv2
+import time
+import threading
 
 queue = []
 
-def recieve_ping():
-    currentTime = time.time()
-    startTime = time.time() - config.get_back_time()
-    endTime = time.time() + config.get_forward_time()
-    queue.append(vc.combine_videos_at_timestamps(startTime, endTime))
+#thread for showing the queue
+class video_shower(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
 
-def video_shower():
+  def run(self):
+    print("video shower started")
+    while True:
+        print(queue)
+        while len(queue) > 0:
+            for video in queue:
+                display_video(video)
+        time.sleep(5)
+
+def recieve_ping():
+    print("ping recieved")
+    currentTime = time.time()
+    start = c.get_start(currentTime)
+    end = c.get_end(currentTime)
+    time.sleep(c.get_forward_time() * 10)
+    queue.append(vc.combine_videos_between_timestamps(start, end))
+
+def display_video(frames):
+    for frame in frames:
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        cv2.waitKey(50)
+
+if __name__ == "__main__" :
+    worker = video_shower()
+    worker.start()
+    recieve_ping()
+    time.sleep(30)
+    recieve_ping()
