7c8be0092cff9ba3eb5492fef24694bb90d8ca7f
Cary <yepster1@live.co.za>
2019-06-10 09:20:48 +0200

    added basic config file with some config variables set

---
+++ src/config.py
@@ -0,0 +1,13 @@
+class config:
+
+    def get_snippit_lengths():
+        return 10
+
+    def get_back_time():
+        return 15
+
+    def get_forward_time():
+        return 15
+
+    def get_raw_directory():
+        return "../data/"
